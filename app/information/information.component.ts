e29a742e3c7df715ddb304bdb1fe4eacd6bc460b
Mehedi-ICE16 <mehedi.ice.16@gmail.com>
2024-03-10 15:26:52 +0600

    first commit

---
+++ app/information/information.component.ts
@@ -0,0 +1,53 @@
+import { Component ,OnInit } from '@angular/core';
+import { FormGroup, FormBuilder,Validators } from '@angular/forms';
+import { CustomValidators } from '../Validators/noSpaceAllowed.validator';
+import { FileUploadService } from "../services/file-upload.service";
+
+@Component({
+  selector: 'app-information',
+  templateUrl: './information.component.html',
+  styleUrl: './information.component.css'
+})
+export class InformationComponent implements OnInit {
+
+  currentPage: number = 1;
+  receivedData:any;
+
+  infoForm!: FormGroup;
+  constructor(private fb: FormBuilder,private fileUploadService: FileUploadService) { 
+    this.fileUploadService.data$.subscribe(data => {
+      this.receivedData = data;
+    });
+   }
+
+  ngOnInit() {
+    this.infoForm = this.fb.group({
+      firstName: this.fb.control(this.receivedData.firstName, [Validators.required, CustomValidators.noSpaceAllowed]),
+      lastName: this.fb.control(this.receivedData.lastName, [Validators.required, CustomValidators.noSpaceAllowed]),
+      mobile: this.fb.control(this.receivedData.mobile, [
+        Validators.required,
+        Validators.minLength(11),
+        Validators.maxLength(11),
+        CustomValidators.noSpaceAllowed
+      ]),
+      nid: this.fb.control(this.receivedData.nid, [Validators.required, CustomValidators.noSpaceAllowed]),
+      address: this.fb.control(this.receivedData.address, Validators.required),
+      email: this.fb.control(this.receivedData.email, [
+        Validators.required,
+        Validators.email,
+        CustomValidators.noSpaceAllowed
+      ]),
+    });
+  }
+
+  keyPress(event: Event) {
+      this.pageChange(4);
+  }
+
+  pageChange(page: number) {
+    this.currentPage = page;
+  }
+  onSubmit() {
+    console.log(this.infoForm.value);
+  }
+}
---
+++ app/services/file-upload.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Observable,BehaviorSubject } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileUploadService {
+
+  private dataSubject = new BehaviorSubject<any>(null);
+  data$ = this.dataSubject.asObservable();
+
+  constructor(private http: HttpClient) { }
+  fetchFile(id:string) {
+  console.log('fetching file');
+   const res = this.http.get('http://localhost:3000/file/'+id, { responseType: 'blob' });
+   console.log(res);
+   return res;
+  }
+
+  uploadFile(file: File): Observable<any> {
+    // const headers = new HttpHeaders({'Content-Type': 'multipart/form-data'});
+    const formData: FormData = new FormData();
+
+    console.log(file);
+
+    console.log(formData);
+    formData.append('fileName', file.name);
+    formData.append('file', file);
+    return this.http.post<any>('http://localhost:3000/file', formData).pipe(
+      catchError(err => {
+         return err;
+      })
+      );  
+  }
+
+  sendData(data: any) {
+    this.dataSubject.next(data);
+  }
+}
